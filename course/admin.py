83c30e253d9d8533f420e057fe5c15a3ab61da40
metoy <xpb.mrzt@gmail.com>
2023-04-14 02:02:53 +0600

    fixed minor bugs, added images dir to ignore

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from course.models import Course
+from course.models import Course, CourseImages
 
 # Register your models here.
 admin.site.register(Course)
+admin.site.register(CourseImages)
--- course/models.py
+++ course/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def get_directory_path(ins, file):
+    return 'images/courses/cover/user_{0}/{1}'.format(ins.user.id, file)
+
+
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=250, unique=True)
@@ -16,26 +20,32 @@ class Course(models.Model):
     languages = models.ForeignKey(Language, on_delete=models.RESTRICT, related_name='languages')
     mentors = models.ForeignKey(Mentor, on_delete=models.RESTRICT, related_name='mentors')
     duration_months = models.IntegerField(default=3)
-    cover = models.ImageField(upload_to='images/', null=True)
+    cover = models.ImageField(upload_to=get_directory_path, null=True, blank=True)
     favorite = models.ManyToManyField(User, related_name='favorites', blank=True)
 
     def __str__(self):
         return f'{self.title}  - {self.category}'
 
-
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
 
+
 class CourseImages(models.Model):
     title = models.CharField(max_length=150, blank=True)
     image = models.ImageField(upload_to='images/')
-    Course = models.ForeignKey(Course, related_name='images', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='images', on_delete=models.CASCADE)
 
     def generate_name(self):
         from random import randint
-        return 'image' + str(self.id) + str(randint(100000, 1_000_000))
+        return 'image' + str(self.title) + str(randint(100000, 1_000_000))
 
     def save(self, *args, **kwargs):
         self.title = self.generate_name()
         return super(CourseImages, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '{} || {}'.format(self.title, self.course)
+
+    class Meta:
+        verbose_name_plural = 'Course Images'

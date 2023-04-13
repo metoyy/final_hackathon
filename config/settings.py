ab606df7e6e87be0d4445d7f0cc4023be737daed
metoy <xpb.mrzt@gmail.com>
2023-04-14 02:19:15 +0600

    Added media root and url

--- config/settings.py
+++ config/settings.py
@@ -172,6 +172,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

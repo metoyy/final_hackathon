c5158f86aac4364b46aa95e986e2af833cb93d3a
metoyy <xpb.mrzt@gmail.com>
2023-04-16 04:07:15 +0600

    modified TGBOT and parsing views

--- parsing/models.py
+++ parsing/models.py
@@ -6,6 +6,7 @@ class Call(models.Model):
     number = models.CharField(max_length=250)
     question = models.TextField(default='I need call')
     date_added = models.DateTimeField(default=now, editable=False)
+    telegram_user = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return '{}  ||  {}'.format(self.number, self.question)

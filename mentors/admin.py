f80b0915e5fb98c66ad0b8b883983f04ec3059ed
metoyy <xpb.mrzt@gmail.com>
2023-04-16 03:48:09 +0600

    Customized Admin panel

--- mentors/admin.py
+++ mentors/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from mentors.models import Mentor
+from course.models import Course
 
 # Register your models here.
-admin.site.register(Mentor)
+@admin.register(Mentor)
+class Mentar(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'years_exp', 'language', 'courses_count')
+    list_filter = ('language',)
+    search_fields = ('first_name', 'last_name', 'language')
+
+    def courses_count(self, obj):
+        mentor = Mentor.objects.get(id=obj.id)
+        courses = Course.objects.filter(mentors=mentor).count()
+        return courses
--- parsing/models.py
+++ parsing/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Call(models.Model):
     number = models.CharField(max_length=250)
     question = models.TextField(default='I need call')
+    date_added = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
         return '{}  ||  {}'.format(self.number, self.question)
